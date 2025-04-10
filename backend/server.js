94a3661da711deb3e8a5dc2e4e2f537ee464975d
puneethsgit <princepuneeths814@gmail.com>
2025-04-11 00:53:18 +0530

    update readme md

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,10 @@
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 
 const app = express();
 
 // parse requests of content-type - application/json
 app.use(express.json());
-
-
-//cros
-app.use(cors()); 
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
@@ -35,7 +31,7 @@ app.get("/", (req, res) => {
 require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080 ;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
